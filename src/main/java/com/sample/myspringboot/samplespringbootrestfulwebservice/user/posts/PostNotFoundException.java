9a115f66e31126d7b586697f8594b3667ad00e82
RaghavChamarthy2112 <raghav.chamarthy@gmail.com>
2018-11-12 12:59:18 +1100

    added code

--- src/main/java/com/sample/myspringboot/samplespringbootrestfulwebservice/user/posts/PostNotFoundException.java
+++ src/main/java/com/sample/myspringboot/samplespringbootrestfulwebservice/user/posts/PostNotFoundException.java
@@ -12,6 +12,7 @@ public class PostNotFoundException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 
 	public PostNotFoundException(String message) {
+		
 		super(message);
 		// TODO Auto-generated constructor stub
 	}
