801be329087058cbcaa7f7cf1cfd3c4bc7df6712
RaghavChamarthy2112 <raghav.chamarthy@gmail.com>
2018-11-12 13:05:22 +1100

    changes to postnotfound exception

--- src/main/java/com/sample/myspringboot/samplespringbootrestfulwebservice/user/posts/PostNotFoundException.java
+++ src/main/java/com/sample/myspringboot/samplespringbootrestfulwebservice/user/posts/PostNotFoundException.java
@@ -12,7 +12,6 @@ public class PostNotFoundException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 
 	public PostNotFoundException(String message) {
-		
 		super(message);
 		// TODO Auto-generated constructor stub
 	}
